06c310e9ff5b490475ec102dd921ed6dfbe06ddf
john kerama <johnnesta2018@gmail.com>
2023-12-13 18:14:48 +0300

    remove unnecessary interceptor

--- server/app.js
+++ server/app.js
@@ -26,7 +26,7 @@ app.use(cors());
 app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
-app.use(express.static(path.join("./", 'public')));
+// app.use(express.static(path.join("./", 'public')));
 
 
 app.use('/database', DatabaseRoute.default);
